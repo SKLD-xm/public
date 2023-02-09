5c2b7af211c9cf0a0a976668e0e8914c8a4340cb
SKLD <109884864+SKLD-xm@users.noreply.github.com>
2023-02-09 13:40:36 -0800

    Update stand-env.lua

--- stand-env.lua
+++ stand-env.lua
@@ -1,3 +1,3 @@
 local main = {
-	["CrazyDiamond"] = {"barrage", "standappear", "healbarrageothers", "healbarrage", "block", "strongpunch", "ohblast", "overwrite", "slam", "movement", "timestop", "posefunc", "u76", "taunt", "dodge"}
+	["SPOH"] = {"barrage", "standappear", "healbarrageothers", "healbarrage", "block", "strongpunch", "ohblast", "overwrite", "slam", "movement", "timestop", "posefunc", "u76", "taunt", "dodge"}
 }
